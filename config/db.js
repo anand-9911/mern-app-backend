a5c9de12e5ec7d12efe509da47da14472529d018
Anand Gautam <GautamAnand@JohnDeere.com>
2020-05-20 10:41:57 +0530

    Addind db deperacation

--- config/db.js
+++ config/db.js
@@ -3,21 +3,18 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-
-    try {
-        await mongoose.connect(
-            db,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true
-            }
-        );
-        console.log('Mongodb Connected');
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);//Exit process with faliure
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('Mongodb Connected');
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1); //Exit process with faliure
+  }
+};
 
 module.exports = connectDB;

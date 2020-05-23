3069b98d09a2dedfb39501c0c611ee095d18be48
Anand Gautam <GautamAnand@JohnDeere.com>
2020-05-23 14:52:56 +0530

    Fixing POST routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -182,27 +182,29 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
+    // Pull out comment
     const comment = post.comments.find(
       (comment) => comment.id === req.params.comment_id
     );
-
-    //Make sure comment exists
+    // Make sure comment exists
     if (!comment) {
-      return res.status(404).json({ mgs: 'Comment does not exists' });
+      return res.status(404).json({ msg: 'Comment does not exist' });
     }
-    if (res.user.id !== comment.user.toString()) {
-      return res.status(401).json({ mgs: 'User not authorized' });
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
     }
-    const removeIndex = post.comments
-      .map((comment) => comment.user.toString())
-      .indexOf(req.user.id);
-    post.comments.splice(removeIndex, 1);
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
     await post.save();
-    res.json(post.comments);
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send('server Error');
+
+    return res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
   }
 });
-
 module.exports = router;

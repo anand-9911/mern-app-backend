1012424bc7bbc31d7b1e1ed6903915677f650da8
Anand Gautam <GautamAnand@JohnDeere.com>
2020-06-05 10:01:03 +0530

    included not found page

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <h1 className='x-large text primary'>
+        <i className='fas fa-exclamation-triangle'>Page Not NotFound</i>
+      </h1>
+    </>
+  );
+};
+
+export default NotFound;
--- client/src/components/post/Discussion.js
+++ client/src/components/post/Discussion.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getPostById } from '../../actions/post';
 import PropTypes from 'prop-types';
